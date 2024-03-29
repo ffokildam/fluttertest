160c79a6224a8ac1d3ccf8995b4ff352d0afc28a
alexandercray05@gmail.com <alexandercray05@gmail.com>
2024-03-29 14:33:52 +0300

    asdsa

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('Test'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              count++;
+            });
+          },
+        ),
+        body: Center(
+          child: Text('$count',style: TextStyle(fontSize: 100)),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, // Fixed
+          backgroundColor: Colors.black, // <-- This works for fixed
+          selectedItemColor: Colors.greenAccent,
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Call',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: 'Message',
+            ),
+          ],
+        ),
+        drawer: const Drawer(
+          child: Text('Salam'),
+        ),
+      ),
+    );
+  }
+}
